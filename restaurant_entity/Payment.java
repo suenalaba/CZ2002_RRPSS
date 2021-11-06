c600cd37fa68a4f4f1224623f2883dd4bf6c932e
Joshua <61338348+suenalaba@users.noreply.github.com>
2021-11-07 00:32:45 +0800

    Add files via upload

---
+++ restaurant_entity/Payment.java
@@ -0,0 +1,124 @@
+package restaurant_entity;
+
+import java.util.Date; //remove
+import java.util.ArrayList;
+import java.text.SimpleDateFormat; //delete
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;  
+//import restaurant_entity.Order;
+//import java.util.Calendar;
+//import java.text.SimpleDateFormat;
+
+public class Payment {
+	private int paymentID;
+	private String paymentDate;
+	private double paymentafterTax;
+	private double totalPayment;
+	private int customerID;
+	private int orderID;
+	private ArrayList<Order> orderlist;
+	private int reservationNumber;
+	private int tableNumber;
+	boolean membershipApplied;
+    private static int runningCount = 1;
+    //private String date;
+    //SimpleDateFormat date_formatting = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss");
+    //int paymentId, ArrayList<Order> orders, String reservationNum, String date
+    
+    public Payment(int reservationNumber, int tableNumber, ArrayList<Order> orderlist) {
+    	this.paymentID = runningCount;
+    	runningCount++;
+    	this.reservationNumber = reservationNumber;
+    	this.tableNumber = tableNumber;
+        this.orderlist = orderlist;
+        LocalDateTime datetimenow = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String paymentDate = datetimenow.format(format);
+        this.paymentDate = paymentDate;
+    }
+    LocalDateTime datetime1 = LocalDateTime.now();  
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
+    String formatDateTime = datetime1.format(format);   
+    
+    public Payment(int paymentID, ArrayList<Order> orderlist, int reservationNumber, int tableNumber,boolean membershipApplied, String paymentDate) {
+    	this.paymentID = runningCount;
+    	//Calendar c = Calendar.getInstance();
+    	//this.paymentDate = paymentDate;
+    	//this.customerID = customerID;
+        this.orderlist = orderlist;
+        this.reservationNumber = reservationNumber;
+        this.tableNumber = tableNumber;
+        this.membershipApplied = membershipApplied;
+        this.paymentDate = paymentDate;
+        //String d = sdf.format(c.getTime());
+        runningCount++;
+    }
+    
+    public int getpaymentID() {
+    	return paymentID;
+    }
+ // no need to set, settle by running count public void setpaymentID(
+    public String getpaymentDate() {
+    	return paymentDate;
+    }
+    
+    public void setpaymentDate(String paymentDate) {
+    	this.paymentDate = paymentDate;
+    }
+    
+    public double getpaymentafterTax() {
+    	return paymentafterTax;
+    }
+    
+    public void setpaymentafterTax(double paymentafterTax) {
+    	this.paymentafterTax = paymentafterTax;
+    }
+    
+    public double gettotalPayment() {
+    	return totalPayment;
+    }
+    
+    public void settotalPayment(double totalPayment) {
+    	this.totalPayment = totalPayment;
+    }
+
+    public int getcustomerID() {
+    	return customerID;
+    }
+    //no need set customerid, this is not done here, should be in customer class??
+    
+    public int getorderID() {
+    	return orderID;
+    }
+    
+    //no need set order id, this is not done here, should be in order class??
+    
+    public ArrayList<Order> getOrderList(){
+    	return orderlist;
+    } //need order class
+    
+    public int getreservationNumber() {
+    	return reservationNumber;
+    }//need reservation class
+    
+    public int gettableNumber() {
+    	return tableNumber;
+    }
+    
+    public boolean getmembershipApplied () {
+    	return membershipApplied;
+    }
+    
+    public void setmembershipApplied(boolean membershipApplied) {
+    	this.membershipApplied = membershipApplied;
+    }
+
+    public static int getrunningCount() {
+    	return runningCount;
+    }
+    
+    public static void setrunningCount(int paymentID) {
+    	runningCount = paymentID;
+    }
+
+}
