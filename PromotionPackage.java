76d4f2d948da409abfb3658dbb3a80eca9af3e9a
parellum <80085511+parellum@users.noreply.github.com>
2021-11-06 11:33:02 +0800

    Update PromotionPackage.java

--- PromotionPackage.java
+++ PromotionPackage.java
@@ -14,15 +14,16 @@ public class PromotionPackage extends MenuItem{
 	}
 	public void printAll() {
 		System.out.format("ItemID: %03d\n",this.getMenuItemID());
-		System.out.format("Name: %s\n", this.getMenuItemName());
-		System.out.format("Item Type: %s\n",this.getMenuItemType().toString());
-		System.out.format("Price: %.02f\n",this.getMenuItemPrice());
-		System.out.format("Description: %s\n", this.getMenuItemDescription());
+		System.out.format("Name: %s\n", this.getMenuItemName().substring(0,1).toUpperCase()+this.getMenuItemName().substring(1).toLowerCase());
+		System.out.format("Item Type: %s\n",this.getMenuItemType().toString().substring(0,1).toUpperCase()+this.getMenuItemType().toString().substring(1).toLowerCase());
+		System.out.format("Price: $%.02f\n",this.getMenuItemPrice());
+		System.out.format("Description: %s\n", this.getMenuItemDescription().substring(0,1).toUpperCase()+this.getMenuItemDescription().substring(1).toLowerCase());
 		System.out.println("This package contains:-------------");
 		for (int i=0;i<this.listOfPromotionPackageItems.size();i++) {
 			System.out.format("ItemID: %03d\n",this.listOfPromotionPackageItems.get(i).getMenuItemID());
-			System.out.format("Name: %s\n", this.listOfPromotionPackageItems.get(i).getMenuItemName());
+			System.out.format("Name: %s\n", this.listOfPromotionPackageItems.get(i).getMenuItemName().substring(0,1).toUpperCase()+this.listOfPromotionPackageItems.get(i).getMenuItemName().substring(1).toLowerCase());
 		}
 		System.out.println("-----------------------------------");
 	}
 }
+
