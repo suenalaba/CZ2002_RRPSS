9c161fd5843bfaf92bb8a495ec25011ce8aefcfa
parellum <80085511+parellum@users.noreply.github.com>
2021-11-06 09:08:55 +0800

    Update PromotionPackage.java

--- PromotionPackage.java
+++ PromotionPackage.java
@@ -1,15 +1,15 @@
 package restaurant_entity;
-
+import java.util.ArrayList;
 public class PromotionPackage extends MenuItem{
-	private MenuItem[] listOfPromotionPackageItems;
-	public PromotionPackage(String name,String description, type itemType,double price,MenuItem[] listOfPromotionPackageItems) {
+	private ArrayList<MenuItem> listOfPromotionPackageItems;
+	public PromotionPackage(String name,String description, type itemType,double price,ArrayList<MenuItem> listOfPromotionPackageItems) {
 		super(name,description,itemType,price);
 		this.listOfPromotionPackageItems=listOfPromotionPackageItems;
 	}
-	public MenuItem[] getPromotionPackageItems() {
+	public ArrayList<MenuItem> getPromotionPackageItems() {
 		return this.listOfPromotionPackageItems;
 	}
-	public void setPromotionPackageItems(MenuItem[] listOfPromotionPackageItems) {
+	public void setPromotionPackageItems(ArrayList<MenuItem> listOfPromotionPackageItems) {
 		this.listOfPromotionPackageItems=listOfPromotionPackageItems;
 	}
 	public void printAll() {
@@ -18,11 +18,11 @@ public class PromotionPackage extends MenuItem{
 		System.out.format("Item Type: %s\n",this.getMenuItemType().toString());
 		System.out.format("Price: %.02f\n",this.getMenuItemPrice());
 		System.out.format("Description: %s\n", this.getMenuItemDescription());
-		System.out.println("This package contains:-----------------------");
-		for (int i=0;i<this.listOfPromotionPackageItems.length;i++) {
-			System.out.format("ItemID: %03d\n",this.listOfPromotionPackageItems[i].getMenuItemID());
-			System.out.format("Name: %s\n", this.listOfPromotionPackageItems[i].getMenuItemName());
+		System.out.println("This package contains:-------------");
+		for (int i=0;i<this.listOfPromotionPackageItems.size();i++) {
+			System.out.format("ItemID: %03d\n",this.listOfPromotionPackageItems.get(i).getMenuItemID());
+			System.out.format("Name: %s\n", this.listOfPromotionPackageItems.get(i).getMenuItemName());
 		}
-		System.out.println("---------------------------------------------");
+		System.out.println("-----------------------------------");
 	}
 }
