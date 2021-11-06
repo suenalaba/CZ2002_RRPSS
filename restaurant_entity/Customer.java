de1d58b1c1638ef7ab9a3aac3a0b7ff9c75ccd9f
Joshua <61338348+suenalaba@users.noreply.github.com>
2021-11-06 19:25:33 +0800

    latest customer class 6 nov 7.25pm

---
+++ restaurant_entity/Customer.java
@@ -0,0 +1,82 @@
+package restaurant_entity;
+
+public class Customer {
+	private String customerID;
+	private String customerName;
+	private String customerGender;
+	private String phoneNumber;
+	private boolean restaurantMembership;
+	private boolean partnerMembership;
+	//private static int runningCount = 1;
+	
+	public Customer() {
+		
+	}
+	
+	public Customer(String customerID, String customerName, String customerGender, String phoneNumber, boolean restaurantMembership,
+			boolean partnerMembership) {
+		this.customerID = customerID;
+		this.customerName = customerName;
+		this.customerGender = customerGender;
+		this.phoneNumber = phoneNumber;
+		this.restaurantMembership = restaurantMembership;
+		this.partnerMembership = partnerMembership;
+		//runningCount++;
+	}
+
+	public String getcustomerID() {
+		return customerID;
+	}
+	
+	public void setcustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+	
+	public String getcustomerName() {
+		return customerName;
+	}
+
+	public void setcustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getcustomerGender() {
+		return customerGender;
+	}
+
+	public void setcustomerGender(String customerGender) {
+		this.customerGender = customerGender;
+	}
+	
+	public String getphoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setphoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public boolean getrestaurantMembership() {
+		return restaurantMembership;
+	}
+	
+	public void setrestaurantMembership(boolean restaurantMembership) {
+		this.restaurantMembership = restaurantMembership;
+	}
+	
+	public boolean getpartnerMembership() {
+		return partnerMembership;
+	}
+	
+	public void setpartnerMembership(boolean partnerMembership) {
+		this.partnerMembership = partnerMembership;
+	}
+	
+    /*public static int getrunningCount() {
+    	return runningCount;
+    }
+    
+    public static void setrunningCount(int customerID) {
+    	runningCount = customerID;
+    }*/
+}
