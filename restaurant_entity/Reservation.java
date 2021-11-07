13bd92887bf2573fa3fe1114415fb47333e2a57b
parellum <80085511+parellum@users.noreply.github.com>
2021-11-08 06:03:07 +0800

    Update Reservation.java

--- restaurant_entity/Reservation.java
+++ restaurant_entity/Reservation.java
@@ -1,82 +1,46 @@
-package RRPSS;
+package restaurant_entity;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Reservation {
 	
+	//Attributes
 	private static int counter = 0;
-	
-	private int reservationID; //auto generated in system.
+	private int reservationID;
 	private int tableID; 
 	private int pax;
-	
 	private String customerID;
-	private String customerName;
-	private String customerContactNum;
-	
-	//private LocalDateTime reservationDate;
 	private LocalDateTime reservationStartTime;
 	private LocalDateTime reservationEndTime;
+	private boolean isFinished;
 	
-	private boolean isFinished; //status finished 
 	
-	
+	//Constructors
 	public Reservation() {
 		this.reservationID = counter;
 		counter++;
-		/*
-		this.reservationID = reservationID;
-		this.customerName = name;
-		this.customerContactNum = contact;
-		this.pax = pax;
-		this.reservationDate = date;
-		this.reservationStartTime = startTime;
-		/* add one hour to start time to signify end time: 
-		this.reservationEndTime = startTime.plusHours(1);
-		*/
+		this.isFinished=false;
 	}
 	
+	public Reservation(String customerID,int pax, LocalDateTime startTime,int tableID) {
+		this.reservationID = counter;
+		counter++;
+		this.customerID=customerID;
+		this.pax=pax;
+		this.reservationStartTime=startTime;
+		this.reservationEndTime=startTime.plusHours(1);
+		this.tableID=tableID;
+		this.isFinished=false;
+	}
 	
+	//methods
 	public int getReservationID(){
 		return reservationID;
 	}
 	
 	public void setReservationID(int reservationID) {
 		this.reservationID = reservationID;
-	}
-	
-	public String getCustomerName(){
-		return customerName;
-	}
-	
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-	
-	public String getCustomerContactNum(){
-		return customerContactNum;
-	}
-	
-	public void setCustomerContactNum(String customerContactNum) {
-		this.customerContactNum = customerContactNum;
-	}
-	
-	
-	public LocalDateTime getReservationStartTime(){
-		return reservationStartTime;
-	}
-	
-	public void setReservationStartTime(LocalDateTime startDateTime) {
-		this.reservationStartTime = startDateTime;
-	}
-	
-	public LocalDateTime getReservationEndTime(){
-		return reservationEndTime;
-	}
-	
-	public void setReservationEndTime(LocalDateTime reservationEndTime) {
-		this.reservationEndTime = reservationEndTime;
 	}
 	
 	public int getTableID(){
@@ -94,26 +58,7 @@ public class Reservation {
 	public void setPax(int pax) {
 		this.pax = pax;
 	}
-
-
-	public boolean getIsFinished() {
-		return isFinished = false;
-	}
-
-
-	public void setIsFinished(boolean status) {
-		this.isFinished = status;
-	}
 	
-	public int getCounter() {
-		return counter;
-	}
-	
-	public static void setCounter(int newCounter) {
-		counter = newCounter;
-	}
-
-
 	public String getCustomerID() {
 		return customerID;
 	}
@@ -123,6 +68,160 @@ public class Reservation {
 		this.customerID = customerID;
 	}
 	
+	public LocalDateTime getReservationStartTime(){
+		return reservationStartTime;
+	}
 	
+	public void setReservationStartTime(LocalDateTime startDateTime) {
+		this.reservationStartTime = startDateTime;
+	}
+	
+	public LocalDateTime getReservationEndTime(){
+		return reservationEndTime;
+	}
+	
+	public void setReservationEndTime(LocalDateTime reservationEndTime) {
+		this.reservationEndTime = reservationEndTime;
+	}
+
+	public boolean getIsFinished() {
+		return isFinished;
+	}
+
+
+	public void setIsFinished(boolean status) {
+		this.isFinished = status;
+	}
+	
+	public static int getCounter() {
+		return counter;
+	}
+	
+	public static void setCounter(int newCounter) {
+		counter = newCounter;
+	}
 	
 }
+// public class Reservation {
+	
+// 	private static int counter = 0;
+	
+// 	private int reservationID; //auto generated in system.
+// 	private int tableID; 
+// 	private int pax;
+	
+// 	private String customerID;
+// 	private String customerName;
+// 	private String customerContactNum;
+	
+// 	//private LocalDateTime reservationDate;
+// 	private LocalDateTime reservationStartTime;
+// 	private LocalDateTime reservationEndTime;
+	
+// 	private boolean isFinished; //status finished 
+	
+	
+// 	public Reservation() {
+// 		this.reservationID = counter;
+// 		counter++;
+// 		/*
+// 		this.reservationID = reservationID;
+// 		this.customerName = name;
+// 		this.customerContactNum = contact;
+// 		this.pax = pax;
+// 		this.reservationDate = date;
+// 		this.reservationStartTime = startTime;
+// 		/* add one hour to start time to signify end time: 
+// 		this.reservationEndTime = startTime.plusHours(1);
+// 		*/
+// 	}
+	
+	
+// 	public int getReservationID(){
+// 		return reservationID;
+// 	}
+	
+// 	public void setReservationID(int reservationID) {
+// 		this.reservationID = reservationID;
+// 	}
+	
+// 	public String getCustomerName(){
+// 		return customerName;
+// 	}
+	
+// 	public void setCustomerName(String customerName) {
+// 		this.customerName = customerName;
+// 	}
+	
+// 	public String getCustomerContactNum(){
+// 		return customerContactNum;
+// 	}
+	
+// 	public void setCustomerContactNum(String customerContactNum) {
+// 		this.customerContactNum = customerContactNum;
+// 	}
+	
+	
+// 	public LocalDateTime getReservationStartTime(){
+// 		return reservationStartTime;
+// 	}
+	
+// 	public void setReservationStartTime(LocalDateTime startDateTime) {
+// 		this.reservationStartTime = startDateTime;
+// 	}
+	
+// 	public LocalDateTime getReservationEndTime(){
+// 		return reservationEndTime;
+// 	}
+	
+// 	public void setReservationEndTime(LocalDateTime reservationEndTime) {
+// 		this.reservationEndTime = reservationEndTime;
+// 	}
+	
+// 	public int getTableID(){
+// 		return tableID;
+// 	}
+	
+// 	public void setTableID(int tableID) {
+// 		this.tableID = tableID;
+// 	}
+	
+// 	public int getPax(){
+// 		return pax;
+// 	}
+	
+// 	public void setPax(int pax) {
+// 		this.pax = pax;
+// 	}
+
+
+// 	public boolean getIsFinished() {
+// 		return isFinished = false;
+// 	}
+
+
+// 	public void setIsFinished(boolean status) {
+// 		this.isFinished = status;
+// 	}
+	
+// 	public int getCounter() {
+// 		return counter;
+// 	}
+	
+// 	public static void setCounter(int newCounter) {
+// 		counter = newCounter;
+// 	}
+
+
+// 	public String getCustomerID() {
+// 		return customerID;
+// 	}
+
+
+// 	public void setCustomerID(String customerID) {
+// 		this.customerID = customerID;
+// 	}
+	
+	
+	
+// }
