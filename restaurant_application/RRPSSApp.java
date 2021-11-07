3d2e20b38dca3bc31c72821f0297781ab658bd47
jiekai98 <40349116+jiekai98@users.noreply.github.com>
2021-11-07 14:55:25 +0800

    Update RRPSSApp.java

--- restaurant_application/RRPSSApp.java
+++ restaurant_application/RRPSSApp.java
@@ -1,6 +1,7 @@
 package restaurant_application;
 import restaurant_manager.MenuManager;
 import restaurant_manager.OrderManager;
+import restaurant_manager.StaffManager;
 import restaurant_manager.TableLayoutManager;
 import java.util.Scanner;
 
@@ -9,7 +10,7 @@ public class RRPSSApp {
     public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("welcome to rrpss pls input ur choice");
-        System.out.println("(1) Manage Menu\n (2) Manage Order\n (3) Make reservation\n (4) Manage Table\n  (5) Manage Staff\n");
+        System.out.println("(1) Manage Menu\n (2) Manage Order\n (3) Make reservation\n (4) Manage Table\n (5) Manage Staff\n");
         int rrpss_main_choice;
         Scanner rrpss_scanner = new Scanner(System.in);
         do {
@@ -19,7 +20,7 @@ public class RRPSSApp {
             case 1:
             		int menu_choice;    
             		do {
-                    	System.out.println("Choice:\n1.Create Menu Item\n2.Remove Menu Item\n3.Update Menu Item\n4.Print Main Menu\n5.Save Menu Data\n6.Load Menu Data");
+                    	System.out.println("Choice:\n 1.Create Menu Item\n2.Remove Menu Item\n3.Update Menu Item\n4.Print Main Menu\n5.Save Menu Data\n6.Load Menu Data");
                     	Scanner menu_scanner = new Scanner(System.in);
                     	menu_choice = menu_scanner.nextInt();
                     	switch(menu_choice) {
@@ -36,7 +37,7 @@ public class RRPSSApp {
                     		MenuManager.printMainMenu();
                     		break;
 			case 5:
-                    		MenuManager.saveDB();
+                    		//MenuManager.saveDB();
                     		break;
 			case 6:
                     		MenuManager.loadDB();
@@ -50,7 +51,7 @@ public class RRPSSApp {
 		    case 2:
 		    		int order_choice;
 		    		do {
-                    	System.out.println("Choice:\n1.Create Order\n2.Delete Order\n3.Update Order\n4.Print All Orders\n");
+                    	System.out.println("Choice:\n 1.Create Order\n2.Delete Order\n3.Update Order\n4.Print All Orders\n");
                     	Scanner order_scanner = new Scanner(System.in);
                     	order_choice = order_scanner.nextInt();
                     	switch(order_choice) {
@@ -64,7 +65,7 @@ public class RRPSSApp {
                     		OrderManager.updateOrderQuery();
                     		break;
                     	case 4:
-                    		OrderManager.displayOrderList();
+                    		 OrderManager.displayOrderList();
                     		break;
                     	default:
                     		break;
@@ -77,7 +78,7 @@ public class RRPSSApp {
 		    case 4:
 		    		int table_choice;
 		    		do {
-		    			System.out.println("Choice:\n1.Add Table\n2.Remove Table\n3.View Empty Tables\n4.View Occupied Tables\n");
+		    			System.out.println("Choice:\n 1.Add Table\n2.Remove Table\n3.View Empty Tables\n4.View Occupied Tables\n");
 		    			Scanner table_scanner = new Scanner(System.in);
                     	table_choice = table_scanner.nextInt();
                     	switch(table_choice) {
@@ -98,11 +99,11 @@ public class RRPSSApp {
                     	}
 		    		} while(table_choice>=1 && table_choice <=4);
 		    		break;
-			case 5: 
+		    case 5: 
 		    		int staff_choice;
 		    		
 		    		do {
-		    			System.out.println("Choice:\n1. Create a Staff\n");
+		    			System.out.println("Choice:\n 1. Create a Staff\n 2. Remove a staff\n 3. Update staff details\n 4. Display list of staff details");
 		    			Scanner staff_scanner = new Scanner(System.in);
 		    			staff_choice = staff_scanner.nextInt();
 		    			staff_scanner.nextLine();
@@ -111,10 +112,19 @@ public class RRPSSApp {
 		    			case 1: 
 		    				StaffManager.createStaffMemberQuery();
 		    				break;
+		    			case 2:
+		    				StaffManager.removeStaffMemberQuery();
+		    				break;
+		    			case 3: 
+		    				StaffManager.updateStaffMemberQuery();
+		    				break;
+		    			case 4: 
+		    				StaffManager.displayStaffList();
 		    			default:
 		    				break;
+		    			
 		    			}
-		    		}while(staff_choice==1);
+		    		}while(staff_choice>=1 && staff_choice<=4);
 		    		break;
             }
         } while (rrpss_main_choice < 6);
