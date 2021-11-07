c24356eafd0e02f67e822c874e88c40b2ec6423a
parellum <80085511+parellum@users.noreply.github.com>
2021-11-08 06:03:57 +0800

    Update Table.java

--- restaurant_entity/Table.java
+++ restaurant_entity/Table.java
@@ -1,6 +1,8 @@
 package restaurant_entity;
 import java.time.LocalDateTime;
 import java.util.*;
+
+import restaurant_manager.TableLayoutManager;
 public class Table {
 	
 	private int tableID; 
@@ -60,50 +62,3 @@ public class Table {
 		this.hourBlock= hourBlock; 
 	}
 }
-// public class Table {
-	
-// 	private int tableID; 
-// 	private int tableCapacity; 
-// 	private status tableStatus;
-// 	private HashMap<LocalDateTime, Reservation> reservations; 
-	
-// 	public Table() {
-// 	}
-	
-// 	public Table(int tableID, int tableCapacity) {
-// 		this.tableID = tableID; 
-// 		this.tableCapacity = tableCapacity; 
-// 		this.tableStatus = status.EMPTY; 
-// 		this.reservations = new HashMap<>();
-// 	}
-	
-// 	public enum status{
-// 		EMPTY, OCCUPIED, RESERVED
-// 	}
-	
-// 	public int getTableID() {
-// 		return tableID; 
-// 	}
-// 	public void setTableID(int tableID) {
-// 		this.tableID = tableID; 
-// 	}
-// 	public int getTableCapacity(){
-// 		return tableCapacity; 
-// 	}
-// 	public void setTableCapacity(int tableCapacity) {
-// 		this.tableCapacity = tableCapacity; 
-// 	}
-// 	public status getTableStatus(){
-// 		return tableStatus; 
-// 	}
-// 	public void setTableStatus(status tableStatus) {
-// 		this.tableStatus = tableStatus; 
-// 	}
-// 	public HashMap<LocalDateTime, Reservation> getReservations(){
-// 		return this.reservations;
-// 	}
-// 	public void setReservations(HashMap<LocalDateTime, Reservation> reservations) {
-// 		this.reservations= reservations; 
-// 	}
-
-// }
