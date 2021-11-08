39385881fe2f54f6532c417dcfc4ad7b9e394934
yizejoseph <josephlohyize8614@gmail.com>
2021-11-08 16:14:19 +0800

    finish staff database

--- restaurant_database/StaffDatabase.java
+++ restaurant_database/StaffDatabase.java
@@ -102,86 +102,47 @@ import restaurant_entity.Staff.Gender;
 
 
 
-public class StaffDatabase{
-	public static final String DELIMITER = ";";
+public class StaffDatabase {
+	public static final String DELIMITER = ",";
 	
-	public static void fwrite(String saveFileName) {
-		File staffDB = new File(saveFileName);
+	public static ArrayList<Staff> fread(String textfilename) throws IOException {
+
+		ArrayList fileasstring = (ArrayList) FileRead.fread(textfilename);
 		
-		try 
-		{
-			//check if order list is empty?
-			
-			// creates a FileWriter Object with the file. Syntax: FileWriter writer = new FileWriter(file); 
-			FileWriter myWriter = new FileWriter("StaffDB.txt");
-			String pusher = "";
-			
-			for(int i=0;i<StaffManager.getListOfStaffMembers().size();i++)
-			{
-				Staff staffItem = StaffManager.getListOfStaffMembers().get(i);
-				
-				pusher +=String.valueOf(staffItem.getStaffID())+";";
-				pusher +=staffItem.getStaffName()+";";
-				pusher += staffItem.getStaffTitle()+";";
-			
-				pusher +=String.valueOf(staffItem.getStaffGender())+";";
-				
-				pusher+="\n";
-				
-			}
-			
-			myWriter.write(pusher);
-			myWriter.close();
-			System.out.println("Successfully wrote to file");
-		}
-		
-		catch(IOException e)
-		{
-			System.out.println("An error occured when writing to file "+ staffDB.getName());
-			e.printStackTrace();
-		}
-		
-		catch(Exception e)
-		{
-			System.out.println("No data to save!");
-		}
-		
-	}
-	
-	
-	
-	
-	
-	
-	public static ArrayList<Staff> fread(String saveFileName) throws IOException{
-		ArrayList fileasstring = (ArrayList) FileRead.fread(saveFileName);
-		
-		//arraylist to store staff
-		
+		//array to store table
 		ArrayList<Staff> listOfStaffMembers = new ArrayList<>();
-		
-		for(int i=0; i<fileasstring.size(); i++)
-		{
+		for (int i = 0; i < fileasstring.size(); i++) {
 			String data = (String) fileasstring.get(i);
+			// get individual 'fields' of the string separated by the delimiter ','
 			
-			// get individual 'fields' of the string separated by the delimiter ';'
-			StringTokenizer str_tokenizer = new StringTokenizer(data, DELIMITER);
-			
-			int staffId = Integer.parseInt(str_tokenizer.nextToken().trim());															
+			StringTokenizer str_tokenizer = new StringTokenizer(data, DELIMITER); // pass in the string to the string tokenizer
+			int staffID = Integer.parseInt(str_tokenizer.nextToken().trim());
 			String staffName = str_tokenizer.nextToken().trim();
-			String staffTitle = str_tokenizer.nextToken().trim();
-			
-			//get an enum type
-			Staff.Gender staffGender = Staff.Gender.valueOf(str_tokenizer.nextToken().trim());
-			
-			
-			Staff staff = new Staff(staffId, staffName, staffTitle, staffGender);
-			listOfStaffMembers.add(staff);
-			
-			
+			String staffTitle = str_tokenizer.nextToken().trim(); 
+			Gender staffGender = Gender.valueOf(str_tokenizer.nextToken().trim().toUpperCase()); 
+			Staff staff = new Staff(staffID, staffName, staffTitle, staffGender);
+			listOfStaffMembers.add(staff); 
 		}
-		
-		return listOfStaffMembers;
+		return listOfStaffMembers; 
 	}
 	
+	public static void fwrite(String textfilename, ArrayList<Staff> listOfStaffMembers) throws IOException {
+
+		List stafflist = new ArrayList();// array list to store staffdata
+		for (int i = 0; i < listOfStaffMembers.size(); i++) {
+			Staff staff = (Staff) listOfStaffMembers.get(i);
+			StringBuilder staffstring = new StringBuilder();
+			staffstring.append(Integer.toString(staff.getStaffID()));
+			staffstring.append(DELIMITER);
+			staffstring.append(staff.getStaffName());
+			staffstring.append(DELIMITER);
+			staffstring.append(staff.getStaffTitle());
+			staffstring.append(DELIMITER); 
+			staffstring.append(staff.getStaffGender().name());
+			staffstring.append(DELIMITER); 
+			stafflist.add(staffstring.toString());
+		}
+		FileRead.fwrite(stafflist,textfilename);
+	}
 }
+
