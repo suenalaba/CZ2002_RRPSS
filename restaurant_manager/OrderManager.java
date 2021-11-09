63f897cd7adee1e904012a2a841bd127cb7e6533
jiekai98 <40349116+jiekai98@users.noreply.github.com>
2021-11-09 21:52:35 +0800

    Add files via upload

--- restaurant_manager/OrderManager.java
+++ restaurant_manager/OrderManager.java
@@ -10,6 +10,7 @@ import restaurant_database.StaffDatabase;
 import restaurant_entity.Menu;
 import restaurant_entity.MenuItem;
 import restaurant_entity.Order;
+import restaurant_entity.Staff;
 
 public class OrderManager {
 	
@@ -19,6 +20,13 @@ public class OrderManager {
 	
 	private static ArrayList<Order> orderList = new ArrayList<Order>();
 	
+	
+	public static  ArrayList<Order> getOrderList(){
+		return orderList;
+	}
+	public static void setOrderList(ArrayList<Order> wholeOrderList) {
+		orderList = wholeOrderList;
+	}
 
 	public OrderManager() {
 		//empty array list to store orders
@@ -39,105 +47,10 @@ public class OrderManager {
 //	public static Order getOrderInstance() { 
 //		return;
 //	}
-	 //in main menu call create order query, update order query or delete whole order query.
-	 public static void createOrderQuery() {
-		 Scanner sc = new Scanner(System.in);
-		 System.out.println("Enter table ID: ");
-		 
-		 int tableId = -1;
-		 ArrayList <Integer> occupiedTableId = new ArrayList<Integer>();
-		 
-		 //add all tableIDs of occupied tables into arraylist occupiedTableId
-		 for (int i=0; i<TableLayoutManager.getOccupiedTables().size() ; i++)
-		 {
-			 occupiedTableId.add(TableLayoutManager.getOccupiedTables().get(i).getTableID());
-		 }
-		 
-		 //ensure input of tableID matches an occupiedtable.. 
-		 while (tableId==-1) {
-	            try {
-	            	
-	              tableId=sc.nextInt();
-	              sc.nextLine();
-	              
-	              if(occupiedTableId.contains(tableId))
-	              {
-	            	  break;
-	              }
-	              
-	              else
-	              {
-	            	  tableId=-1;
-	            	  System.out.println("Table ID entered is NOT an occupied table. Input again: ");
-	            	  
-	              }
-	              
-	            }
-	            catch(InputMismatchException e) {
-	              sc.nextLine();
-	              System.out.println("Not an Integer. Try Again:");
-	            }
-	          }
-		 
-		 	
-		 	int choiceStop;
-	 		
-		 	
-	 		ArrayList<MenuItem> orderItems = new ArrayList<MenuItem>();
-	 		
-	 		//add item to orderitems list
-		 	do{
-		 		System.out.println("Add item to order");
-		 		MenuItem newItem = MenuManager.getItem();
-		 		orderItems.add(newItem);
-		 		
-		 		System.out.println("Want to add another item? 1 - yes, 2 - No ");
-		 		choiceStop  = sc.nextInt();
-		 		sc.nextLine();
-		 		
-		 		
-		 	}while(choiceStop==1);
-		 
-		 	
-		 	//check if staffID exists based on staffID staff inputs
-		 	System.out.println("Enter your staffID: ");
-		 int staffId =-1;
-		 ArrayList <Integer> staffIdList = new ArrayList<Integer>();
-		 
-		 for(int i=0; i<StaffManager.getListOfStaffMembers().size(); i++)
-		 {
-			 staffIdList.add(StaffManager.getListOfStaffMembers().get(i).getStaffID());
-		 }
-		 while (staffId==-1) {
-	            try {
-	            	
-	              staffId=sc.nextInt();
-	              sc.nextLine();
-	              
-	              if(staffIdList.contains(staffId))
-	              {
-	            	  break;
-	              }
-	              
-	              else
-	              {
-	            	  staffId=-1;
-	            	  System.out.println("Staff ID entered is NOT in system. Input again: ");
-	            	  
-	              }
-	              
-	            }
-	            catch(InputMismatchException e) {
-	              sc.nextLine();
-	              System.out.println("Not an Integer. Try Again:");
-	            }
-	          }
-		 
-		 
-		 createOrder(tableId, orderItems, staffId);
-	 }
+	 
+
 	 //method create new order item 
-	 private static void createOrder(int tableId, ArrayList<MenuItem> orderItems, int staffId) {
+	 public static void createOrder(int tableId, ArrayList<MenuItem> orderItems, int staffId) {
 		 
 		 	
 		 	orderList.add(new Order(tableId, orderItems,staffId));
@@ -166,250 +79,20 @@ public class OrderManager {
 	 	}
 	 
 	 	
-	 	//method to delete the whole entire order based on the order ID asked from staff input
-	 	public static void deleteWholeOrderQuery() {
-	 		System.out.println("Enter orderId you want to delete: ");
-	 		displayOrderList();
-	 		 Scanner sc = new Scanner(System.in);
-	 		int orderIdToDelete = -1;
-	 		//error handle if orderId entered not an integer. 
-	 		
-	 		
-			
-			while (orderIdToDelete==-1)
-			{
-				try {
-					orderIdToDelete = sc.nextInt();
-					sc.nextLine();
-					
-				}
-				
-				catch(InputMismatchException e) {
-					sc.nextLine();
-					System.out.println("Not an Integer. Try Again:");
-				}
-			}
-			
-	 		//check if orderId is in system and go to that particular order to delete from orderList (arraylist)
-	 		for(int i=0; i<orderList.size(); i++)
-	 		{
-	 			if(orderList.get(i).getOrderID()==orderIdToDelete)
-	 			{
-	 				int removalIndex = i;
-	 				deleteWholeOrder(removalIndex);
-	 				return;
-	 			}
-	 		}
-	 		
-	 		
-	 	}
+	 	
 	 	
 	 	
 	 	
 	 	//delete whole order
-	 	private static void deleteWholeOrder(int removalIndex) {
+	 	public static void deleteWholeOrder(int removalIndex) {
 	 		orderList.remove(removalIndex);
 	 	}
 	 	
 	 	
-	 //updateOrder
-	 public static void updateOrderQuery() {
-		 
-		 displayOrderList();
-		 int newTableId=-1;
-		 System.out.println("Enter orderID that you want to update: ");
-		 Scanner sc = new Scanner(System.in);
-		 
-		 int orderIdUpdate = -1;
-		 //error handling if order id to update entered is not an integer. 
-		
-			
-			while (orderIdUpdate==-1)
-			{
-				try {
-					orderIdUpdate = sc.nextInt();
-					sc.nextLine();
-					
-				}
-				
-				catch(InputMismatchException e) {
-					sc.nextLine();
-					System.out.println("Not an Integer. Try Again:");
-				}
-			}
-		 
-		 //check if orderId exists in orderlist and assign orderId to update to that order Id entered
-		 for(int i=0; i<orderList.size(); i++ ) {
-			 if(orderList.get(i).getOrderID() == orderIdUpdate)
-			 {
-				 orderIdUpdate = i;
-				 break;
-			 }
-			 
-		 }
-		 
-		 ArrayList<MenuItem> newOrderItems = orderList.get(orderIdUpdate).getOrderItems();
-		 
-		 System.out.println("Which do you want to update? TableID (1) or orderitems (2)?");
-		 int choice = -1;
-		 
-		 //check for non integer input for choice of updating tableId or orderitems
-		 
-			
-			while (choice==-1)
-			{
-				try {
-					choice = sc.nextInt();
-					sc.nextLine();
-					
-				}
-				
-				catch(InputMismatchException e) {
-					sc.nextLine();
-					System.out.println("Not an Integer. Try Again:");
-				}
-			}
-		 
-		 if(choice==1)
-		 {
-			 System.out.format("Your current tableID is %d.\n What is the new tableID you want to update to?\n", orderList.get(orderIdUpdate).getTableID());
-		
-			 //check if tableID is occupied. 
-			 ArrayList <Integer> occupiedTableId = new ArrayList<Integer>();
-			 
-			 
-			 //check that table Id entered is valid
-			 //add all tableIDs of occupied tables into arraylist occipiedTableId
-			 for (int i=0; i<TableLayoutManager.getOccupiedTables().size() ; i++)
-			 {
-				 occupiedTableId.add(TableLayoutManager.getOccupiedTables().get(i).getTableID());
-			 }
-			 
-			 newTableId = -1;
-			 
-			 while (newTableId==-1) {
-		            try {
-		            	
-		              newTableId=sc.nextInt();
-		              sc.nextLine();
-		              
-		              if(occupiedTableId.contains(newTableId))
-		              {
-		            	  break;
-		              }
-		              
-		              else
-		              {
-		            	  newTableId=-1;
-		            	  System.out.println("table Id entered is not occupied. Input again: ");
-		            	  
-		              }
-		              
-		            }
-		            catch(InputMismatchException e) {
-		              sc.nextLine();
-		              System.out.println("Not an Integer. Try Again:");
-		            }
-		          }
-		 }
-		 
-		 else if(choice==2){
-			 
-			 int deleteOrAdd = 0;
-			 do {
-				 System.out.format("Make a choice:\n 1. Delete Item\n 2. Add Item\n 3.Quit");
-//				 deleteOrAdd = sc.nextInt();
-//				 sc.nextLine();
-				 
-				
-					deleteOrAdd=0;
-					while (deleteOrAdd==0)
-					{
-						try {
-							deleteOrAdd = sc.nextInt();
-							sc.nextLine();
-							
-						}
-						
-						catch(InputMismatchException e) {
-							sc.nextLine();
-							System.out.println("Not an Integer. Try Again:");
-						}
-					}
-				 ArrayList<MenuItem> tempconvert= orderList.get(orderIdUpdate).getOrderItems();
-				 Menu orderItems=new Menu(tempconvert);
-				 orderItems.printMenu();
-				 
-				 switch(deleteOrAdd) {
-				 case 1:
-					 System.out.println("Enter index of order you want to remove: ");
-					 
-					 //run through array of menu list and check for item Id match before returning index of itemID
-					 //false==>wont check deleted items in the menu
-					 int removeID = orderItems.ItemIDToIndex(sc.nextInt(), false);
-					 newOrderItems.remove(removeID);
-					 
-					 
-					 
-					 break;
-				 case 2:
-					 System.out.println("Enter index of order you want to add items to: ");
-					 //returns menu items from Menu
-					 MenuItem newItem = MenuManager.getItem();
-					 
-					 //
-					 int addedCheck=0;
-					 //check if new item is already inside the order. 
-					 for (int i=0; i<newOrderItems.size(); i++)
-					 {
-						 if(newOrderItems.get(i) == newItem)
-						 {
-							 //if item already exists, add another one beside each other
-							 //add(index, item)
-							 addedCheck=1;
-							 int addIndex = i;
-							 newOrderItems.add(addIndex,newItem);
-							 break;
-						 }
-						 
-					 }
-					 
-					 if(addedCheck==0)
-					 {
-						 //item not inside, append item to the end.
-						 newOrderItems.add(newItem);
-					 }
-					 
-					 
-						 
-					 break;
-				default:
-					break;
-					 
-				 }
-			 }while(deleteOrAdd==1 || deleteOrAdd==2);
-			
-			
-			 
-			
-		 }
-		 
-		 
-		 else
-		 {
-			 return;
-		 }
-		 
-		 	if(newTableId==-1)
-		 	{
-		 		newTableId = orderList.get(orderIdUpdate).getTableID();
-		 	}
-		 	printParticularOrder(newTableId);
-		 	updateOrder(newTableId, newOrderItems, orderIdUpdate);
-	 }
 	 
 	 
-	 private static void updateOrder(int newTableId, ArrayList<MenuItem> newOrderItems, int updateIndex)
+	 //update Order
+	 public static void updateOrder(int newTableId, ArrayList<MenuItem> newOrderItems, int updateIndex)
 	 {
 		 orderList.get(updateIndex).setTableID(newTableId);
 		 orderList.get(updateIndex).setOrderItems(newOrderItems);
@@ -417,51 +100,7 @@ public class OrderManager {
 	 }
 	    
 	 
-	 public static void displayOrderBasedOnTableIdQuery() {
-		 Scanner sc = new Scanner(System.in);
-		 System.out.println("Enter tableID of order you want to display:");
-		 
-		 //check if tableID input valid. check if tableID is an occupied table
-		 //check if tableID is occupied. 
-		 ArrayList <Integer> occupiedTableId = new ArrayList<Integer>();
-		 
-		 
-		 //check that table Id entered is valid
-		 //add all tableIDs of occupied tables into arraylist occipiedTableId
-		 for (int i=0; i<TableLayoutManager.getOccupiedTables().size() ; i++)
-		 {
-			 occupiedTableId.add(TableLayoutManager.getOccupiedTables().get(i).getTableID());
-		 }
-		 
-		 int newTableId = -1;
-		 
-		 while (newTableId==-1) {
-	            try {
-	            	
-	              newTableId=sc.nextInt();
-	              sc.nextLine();
-	              
-	              if(occupiedTableId.contains(newTableId))
-	              {
-	            	  break;
-	              }
-	              
-	              else
-	              {
-	            	  newTableId=-1;
-	            	  System.out.println("table Id entered is not occupied. Input again: ");
-	            	  
-	              }
-	              
-	            }
-	            catch(InputMismatchException e) {
-	              sc.nextLine();
-	              System.out.println("Not an Integer. Try Again:");
-	            }
-	          }
-		 
-		 printParticularOrder(newTableId);
-	 }
+
 	 
 	 public static void printParticularOrder(int tableID) {
 		 for(int i=0; i<orderList.size();i++)
@@ -582,7 +221,7 @@ public class OrderManager {
 			}
 		}
 
-	    
+	   
 	 	//save orders to order database
 		public static void saveOrderDB(String saveFileName)  {
 			
--- restaurant_manager/StaffManager.java
+++ restaurant_manager/StaffManager.java
@@ -14,123 +14,287 @@ import restaurant_entity.Staff;
 
 public class StaffManager {
 	
-	//public static final String delimiter = ";";
+	
 
 	
 	private static ArrayList<Staff> listOfStaffMembers = new ArrayList<Staff>();
 	
-	public void setListOfStaffMembers(ArrayList<Staff> listOfStaffMembers) {
-		this.listOfStaffMembers = listOfStaffMembers;
-	}
-	
-	public static ArrayList<Staff> getListOfStaffMembers(){
+	public static  ArrayList<Staff> getListOfStaffMembers(){
 		return listOfStaffMembers;
 	}
-	
-	
-	//create a staff member and add to system
-	public static void createStaffMemberQuery() {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter staff ID: ");
-		
-		//check if staffId already exists as staff Id is unique. 
-		 ArrayList <Integer> existingStaffId = new ArrayList<Integer>();
-		 
-		 //pull out all existing staff Id from listofstaffmembers and add to existingstaffId
-		 for(int i=0; i<listOfStaffMembers.size();i++)
-		 {
-			 existingStaffId.add(listOfStaffMembers.get(i).getStaffID());
-		 }
-		 
-		int staffId = -1;
-				
-				while (staffId==-1)
-				{
-					try {
-						staffId = sc.nextInt();
-						//sc.nextLine();
-						
-						//check if existing list of staffId contains the staffId entered. 
-						if(existingStaffId.contains(staffId))
-						{
-							staffId=-1;
-							System.out.println("Staff Id entered already exists. Please enter another one.");
-						}
-						
-						else
-						{
-							break;
-						}
-						
-					}
-					
-					catch(InputMismatchException e) {
-						sc.nextLine();
-						System.out.println("Not an Integer. Try Again:");
-					}
-				}
-				
-		
-		
-		System.out.println("Enter staff Name: ");
-		String staffName = sc.nextLine();
-		
-		System.out.println("Enter staff title: ");
-		String staffTitle = sc.nextLine();
-		
-		System.out.println("Enter staff gender: Enter 1 for MALE, 2 for FEMALE: ");
-		int genderChoice = -1;
-		
-		while (genderChoice==-1)
-		{
-			try {
-				genderChoice = sc.nextInt();
-				sc.nextLine();
-				
-				if(genderChoice!=1 || genderChoice!=2)
-				{
-					genderChoice=-1;
-					System.out.println("Gender choice invalid. Choose either 1 for MALE or 2 for FEMALE: ");
-				}
-				
-				else
-				{
-					break;
-				}
-				
-			}
-			
-			catch(InputMismatchException e) {
-				sc.nextLine();
-				System.out.println("Not an Integer. Try Again:");
-			}
-		}
-		
-		Gender staffGender;
-		switch(genderChoice) {
-		
-		case 1: staffGender = Gender.MALE;
-		break;
-		
-		case 2: staffGender = Gender.FEMALE;
-		break;
-		
-		default: 
-			staffGender = Gender.MALE;
-			break;
-		}
-		
-		
-//		listOfStaffMembers.set(genderChoice, null)
-		createStaffMember(staffId, staffName,staffTitle,staffGender);
+	public static void setListOfStaffMembers(ArrayList<Staff> wholeListOfStaffMembers) {
+		listOfStaffMembers = wholeListOfStaffMembers;
 	}
 	
-	private static void createStaffMember(int staffId, String staffName, String staffTitle, Gender staffGender) {
+	
+	
+	
+//	//create a staff member and add to system
+//	public static void createStaffMemberQuery() {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter staff ID: ");
+//		
+//		//check if staffId already exists as staff Id is unique. 
+//		 ArrayList <Integer> existingStaffId = new ArrayList<Integer>();
+//		 
+//		 //pull out all existing staff Id from listofstaffmembers and add to existingstaffId
+//		 for(int i=0; i<listOfStaffMembers.size();i++)
+//		 {
+//			 existingStaffId.add(listOfStaffMembers.get(i).getStaffID());
+//		 }
+//		 
+//		int staffId = -1;
+//				
+//				while (staffId==-1)
+//				{
+//					try {
+//						staffId = sc.nextInt();
+//						//sc.nextLine();
+//						
+//						//check if existing list of staffId contains the staffId entered. 
+//						if(existingStaffId.contains(staffId))
+//						{
+//							staffId=-1;
+//							System.out.println("Staff Id entered already exists. Please enter another one.");
+//						}
+//						
+//						else
+//						{
+//							break;
+//						}
+//						
+//					}
+//					
+//					catch(InputMismatchException e) {
+//						sc.nextLine();
+//						System.out.println("Not an Integer. Try Again:");
+//					}
+//				}
+//				
+//		
+//		
+//		System.out.println("Enter staff Name: ");
+//		String staffName = sc.nextLine();
+//		
+//		System.out.println("Enter staff title: ");
+//		String staffTitle = sc.nextLine();
+//		
+//		System.out.println("Enter staff gender: Enter 1 for MALE, 2 for FEMALE: ");
+//		int genderChoice = -1;
+//		
+//		while (genderChoice==-1)
+//		{
+//			try {
+//				genderChoice = sc.nextInt();
+//				sc.nextLine();
+//				
+//				if(genderChoice!=1 || genderChoice!=2)
+//				{
+//					genderChoice=-1;
+//					System.out.println("Gender choice invalid. Choose either 1 for MALE or 2 for FEMALE: ");
+//				}
+//				
+//				else
+//				{
+//					break;
+//				}
+//				
+//			}
+//			
+//			catch(InputMismatchException e) {
+//				sc.nextLine();
+//				System.out.println("Not an Integer. Try Again:");
+//			}
+//		}
+//		
+//		Gender staffGender;
+//		switch(genderChoice) {
+//		
+//		case 1: staffGender = Gender.MALE;
+//		break;
+//		
+//		case 2: staffGender = Gender.FEMALE;
+//		break;
+//		
+//		default: 
+//			staffGender = Gender.MALE;
+//			break;
+//		}
+//		
+//		
+////		listOfStaffMembers.set(genderChoice, null)
+//		createStaffMember(staffId, staffName,staffTitle,staffGender);
+//	}
+//	
+//	
+//	
+//
+//	//remove staff
+//	public static void removeStaffMemberQuery() {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		
+//		//find staffID inside array list
+//		System.out.println("Enter staff ID of staff you want to remove: ");
+//		displayStaffList();
+//		
+//		//check if staffId entered exists in stafflist. else ask user enter again. 
+//		 ArrayList <Integer> existingStaffId = new ArrayList<Integer>();
+//		 
+//		 //pull out all existing staff Id from listofstaffmemebers and add to existingstaffId
+//		 for(int i=0; i<listOfStaffMembers.size();i++)
+//		 {
+//			 existingStaffId.add(listOfStaffMembers.get(i).getStaffID());
+//		 }
+//		
+//		//error handling if input for staffId not an integer
+//		int staffId = -1;
+//		
+//		while (staffId==-1)
+//		{
+//			try {
+//				staffId = sc.nextInt();
+//				sc.nextLine();
+//				
+//				if(existingStaffId.contains(staffId))
+//				{
+//					break;
+//				}
+//				
+//				else
+//				{
+//					
+//					staffId=-1;
+//					System.out.println("Staff Id entered does not exists in system. Please enter another one.");
+//				}
+//				
+//			}
+//			
+//			catch(InputMismatchException e) {
+//				sc.nextLine();
+//				System.out.println("Not an Integer. Try Again:");
+//			}
+//		}
+//		
+//		
+//		//check if staffID exists in listOfStaffMembers
+//		for(int i=0;i<listOfStaffMembers.size(); i++)
+//		{
+//			if(listOfStaffMembers.get(i).getStaffID() == staffId)
+//			{
+//				int removalIndex = i;
+//				removeStaffMember(removalIndex);
+//				return;
+//			}
+//		}
+//		
+//		
+//	}
+//	
+//	
+//	
+//	//update staff details (staffName and staffTitle)
+//	public static void updateStaffMemberQuery() {
+//		
+//		
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter the staff ID of the staff that you want to update details to:");
+//		displayStaffList();
+//		
+//		//check if staffId entered exists in stafflist. else ask user enter again. 
+//		 ArrayList <Integer> existingStaffId = new ArrayList<Integer>();
+//		 
+//		 //pull out all existing staff Id from listofstaffmemebers and add to existingstaffId
+//		 for(int i=0; i<listOfStaffMembers.size();i++)
+//		 {
+//			 existingStaffId.add(listOfStaffMembers.get(i).getStaffID());
+//		 }
+//		
+//		 
+//		int staffIdUpdate = -1;
+//		
+//		while (staffIdUpdate==-1)
+//		{
+//			try {
+//				staffIdUpdate = sc.nextInt();
+//				sc.nextLine();
+//				
+//				if(existingStaffId.contains(staffIdUpdate))
+//				{
+//					break;
+//				}
+//				
+//				else
+//				{
+//					
+//					staffIdUpdate=-1;
+//					System.out.println("Staff Id entered does not exists in system. Please enter another one.");
+//				}
+//				
+//				
+//			}
+//			
+//			catch(InputMismatchException e) {
+//				sc.nextLine();
+//				System.out.println("Not an Integer. Try Again:");
+//			}
+//		}
+//		
+//		
+//		//check if staff Id entered exists in the list of staff members
+//		for(int i=0; i<listOfStaffMembers.size(); i++) {
+//			 if(listOfStaffMembers.get(i).getStaffID() == staffIdUpdate)
+//			 {
+//				 staffIdUpdate= i;
+//				 break;
+//			 }
+//			 
+//		 }
+//		
+//		//ArrayList<Staff> newListOfStaffMembers = listOfStaffMembers.get(staffIdUpdate).
+//		
+//		//change staffName, title
+//		
+//		
+//		System.out.println("Enter updated staff name:");
+//		String updatedStaffName = sc.nextLine();
+//		
+//		System.out.println("Enter updated staff title:");
+//		String updatedStaffTitle = sc.nextLine();
+//		
+//		
+//		
+//		updateStaffMember(updatedStaffName, updatedStaffTitle, staffIdUpdate);
+//	}
+	
+	
+	public static void createStaffMember(int staffId, String staffName, String staffTitle, Gender staffGender) {
 		listOfStaffMembers.add(new Staff(staffId,staffName, staffTitle,staffGender));
 		
 		
 	}
+	
+	public static void removeStaffMember(int removalStaffId) {
+		//list ofstaffmembers.remove(staffID) 
+		
+		listOfStaffMembers.remove(removalStaffId);
+		System.out.println("Staff removed. ");
+	}
+	
+	public static void updateStaffMember(String updatedStaffName, String updatedStaffTitle, int staffIdUpdate) {
+		
+		//listOfStaffMembers.set(staffIdUpdate, updatedStaffName);
+		
+		listOfStaffMembers.get(staffIdUpdate).setStaffName(updatedStaffName);
+		listOfStaffMembers.get(staffIdUpdate).setStaffTitle(updatedStaffTitle);
+		System.out.println("Staff details updated");
+	}
+	
+	
+	
 	
 	//display staffs in the system
 	public static void displayStaffList() {
@@ -142,161 +306,6 @@ public class StaffManager {
  		
  		
  	}
-	//remove staff
-	public static void removeStaffMemberQuery() {
-		Scanner sc = new Scanner(System.in);
-		
-		
-		//find staffID inside array list
-		System.out.println("Enter staff ID of staff you want to remove: ");
-		displayStaffList();
-		
-		//check if staffId entered exists in stafflist. else ask user enter again. 
-		 ArrayList <Integer> existingStaffId = new ArrayList<Integer>();
-		 
-		 //pull out all existing staff Id from listofstaffmemebers and add to existingstaffId
-		 for(int i=0; i<listOfStaffMembers.size();i++)
-		 {
-			 existingStaffId.add(listOfStaffMembers.get(i).getStaffID());
-		 }
-		
-		//error handling if input for staffId not an integer
-		int staffId = -1;
-		
-		while (staffId==-1)
-		{
-			try {
-				staffId = sc.nextInt();
-				sc.nextLine();
-				
-				if(existingStaffId.contains(staffId))
-				{
-					break;
-				}
-				
-				else
-				{
-					
-					staffId=-1;
-					System.out.println("Staff Id entered does not exists in system. Please enter another one.");
-				}
-				
-			}
-			
-			catch(InputMismatchException e) {
-				sc.nextLine();
-				System.out.println("Not an Integer. Try Again:");
-			}
-		}
-		
-		
-		//check if staffID exists in listOfStaffMembers
-		for(int i=0;i<listOfStaffMembers.size(); i++)
-		{
-			if(listOfStaffMembers.get(i).getStaffID() == staffId)
-			{
-				int removalIndex = i;
-				removeStaffMember(removalIndex);
-				return;
-			}
-		}
-		
-		
-	}
-	private static void removeStaffMember(int removalStaffId) {
-		//list ofstaffmembers.remove(staffID) 
-		
-		listOfStaffMembers.remove(removalStaffId);
-		System.out.println("Staff removed. ");
-	}
-	
-	
-	
-	//update staff details (staffName and staffTitle)
-	public static void updateStaffMemberQuery() {
-		
-		
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter the staff ID of the staff that you want to update details to:");
-		displayStaffList();
-		
-		//check if staffId entered exists in stafflist. else ask user enter again. 
-		 ArrayList <Integer> existingStaffId = new ArrayList<Integer>();
-		 
-		 //pull out all existing staff Id from listofstaffmemebers and add to existingstaffId
-		 for(int i=0; i<listOfStaffMembers.size();i++)
-		 {
-			 existingStaffId.add(listOfStaffMembers.get(i).getStaffID());
-		 }
-		
-		 
-		int staffIdUpdate = -1;
-		
-		while (staffIdUpdate==-1)
-		{
-			try {
-				staffIdUpdate = sc.nextInt();
-				sc.nextLine();
-				
-				if(existingStaffId.contains(staffIdUpdate))
-				{
-					break;
-				}
-				
-				else
-				{
-					
-					staffIdUpdate=-1;
-					System.out.println("Staff Id entered does not exists in system. Please enter another one.");
-				}
-				
-				
-			}
-			
-			catch(InputMismatchException e) {
-				sc.nextLine();
-				System.out.println("Not an Integer. Try Again:");
-			}
-		}
-		
-		
-		//check if staff Id entered exists in the list of staff members
-		for(int i=0; i<listOfStaffMembers.size(); i++) {
-			 if(listOfStaffMembers.get(i).getStaffID() == staffIdUpdate)
-			 {
-				 staffIdUpdate= i;
-				 break;
-			 }
-			 
-		 }
-		
-		//ArrayList<Staff> newListOfStaffMembers = listOfStaffMembers.get(staffIdUpdate).
-		
-		//change staffName, title
-		
-		
-		System.out.println("Enter updated staff name:");
-		String updatedStaffName = sc.nextLine();
-		
-		System.out.println("Enter updated staff title:");
-		String updatedStaffTitle = sc.nextLine();
-		
-		
-		
-		updateStaffMember(updatedStaffName, updatedStaffTitle, staffIdUpdate);
-	}
-	
-	private static void updateStaffMember(String updatedStaffName, String updatedStaffTitle, int staffIdUpdate) {
-		
-		//listOfStaffMembers.set(staffIdUpdate, updatedStaffName);
-		
-		listOfStaffMembers.get(staffIdUpdate).setStaffName(updatedStaffName);
-		listOfStaffMembers.get(staffIdUpdate).setStaffTitle(updatedStaffTitle);
-		System.out.println("Staff details updated");
-	}
-	
 	public static void saveStaffDB(String saveFileName)  {
 		
 		try{
